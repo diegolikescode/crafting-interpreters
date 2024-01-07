70ca1215778ed72c4f0dd7f72cbb021021565a9d
diegolikescode <diegolikescode@hotmail.com>
2024-01-07 17:33:54 -0300

    add if statements to the interpreter

--- jlox-core/src/lox/Parser.java
+++ jlox-core/src/lox/Parser.java
@@ -2,6 +2,7 @@ package lox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import static lox.TokenType.*;
 
@@ -27,9 +28,24 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if(match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
