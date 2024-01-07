61f2f98439d73a1aab200b53733c710fb22b8c1b
diegolikescode <diegolikescode@hotmail.com>
2024-01-07 12:00:33 -0300

    new Expressions and Statements for variable declaration

--- jlox-core/src/lox/AstPrinterRPN.java
+++ jlox-core/src/lox/AstPrinterRPN.java
@@ -7,6 +7,11 @@ public class AstPrinterRPN implements Expr.Visitor<String>{
     }
 
     @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
+    @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
