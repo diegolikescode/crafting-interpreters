66fe7b79f9e36dddfcea18d20cf566626b6d1c9f
diegolikescode <diegoprestes.git@hotmail.com>
2024-03-21 00:27:22 -0300

    interpreter resolver

--- jlox-core/src/lox/Lox.java
+++ jlox-core/src/lox/Lox.java
@@ -49,11 +49,15 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
-
         if(hadError) return;
-        // System.out.println(new AstPrinter().print(expression));
+
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        if(hadError) return;
+
         interpreter.interpret(statements);
     }
 
