0a459857771f5eff492f9aef748d253cc48d965f
diegolikescode <diegoprestes.git@hotmail.com>
2024-03-25 20:23:35 -0300

    very simple class structure for jlox

--- jlox-core/src/lox/Stmt.java
+++ jlox-core/src/lox/Stmt.java
@@ -4,41 +4,20 @@ import java.util.List;
 
 abstract class Stmt {
     interface Visitor<R> {
-        R visitExpressionStmt(Expression stmt);
-
         R visitFunctionStmt(Function stmt);
-
         R visitIfStmt(If stmt);
-
         R visitPrintStmt(Print stmt);
-
         R visitReturnStmt(Return stmt);
-
         R visitVarStmt(Var stmt);
-
         R visitWhileStmt(While stmt);
-
         R visitBlockStmt(Block stmt);
+        R visitClassStmt(Class stmt);
+        R visitExpressionStmt(Expression stmt);
     }
-
-    static class Expression extends Stmt {
-        final Expr expression;
-
-        Expression(Expr expression) {
-            this.expression = expression;
-        }
-
-        @Override
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitExpressionStmt(this);
-        }
-    }
-
     static class Function extends Stmt {
         final Token name;
         final List<Token> params;
         final List<Stmt> body;
-
         Function(Token name, List<Token> params, List<Stmt> body) {
             this.name = name;
             this.params = params;
@@ -55,7 +34,6 @@ abstract class Stmt {
         final Expr condition;
         final Stmt thenBranch;
         final Stmt elseBranch;
-
         If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
             this.condition = condition;
             this.thenBranch = thenBranch;
@@ -70,7 +48,6 @@ abstract class Stmt {
 
     static class Print extends Stmt {
         final Expr expression;
-
         Print(Expr expression) {
             this.expression = expression;
         }
@@ -84,7 +61,6 @@ abstract class Stmt {
     static class Return extends Stmt {
         final Token keyword;
         final Expr value;
-
         Return(Token keyword, Expr value) {
             this.keyword = keyword;
             this.value = value;
@@ -99,7 +75,6 @@ abstract class Stmt {
     static class Var extends Stmt {
         final Token name;
         final Expr initializer;
-
         Var(Token name, Expr initializer) {
             this.name = name;
             this.initializer = initializer;
@@ -114,7 +89,6 @@ abstract class Stmt {
     static class While extends Stmt {
         final Expr condition;
         final Stmt body;
-
         While(Expr condition, Stmt body) {
             this.condition = condition;
             this.body = body;
@@ -128,7 +102,6 @@ abstract class Stmt {
 
     static class Block extends Stmt {
         final List<Stmt> statements;
-
         Block(List<Stmt> statements) {
             this.statements = statements;
         }
@@ -136,6 +109,32 @@ abstract class Stmt {
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBlockStmt(this);
+        }
+    }
+
+    static class Class extends Stmt {
+        final Token name;
+        final List<Stmt.Function> methods;
+        Class(Token name, List<Stmt.Function> methods) {
+            this.name = name;
+            this.methods = methods;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitClassStmt(this);
+        }
+    }
+
+    static class Expression extends Stmt {
+        final Expr expression;
+        Expression(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
         }
     }
 
--- jlox-core/src/tool/GenerateAst.java
+++ jlox-core/src/tool/GenerateAst.java
@@ -32,14 +32,15 @@ public class GenerateAst {
         ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
-                "Expression : Expr expression",
                 "Function   : Token name, List<Token> params, List<Stmt> body",
                 "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print      : Expr expression",
                 "Return     : Token keyword, Expr value",
                 "Var        : Token name, Expr initializer",
                 "While      : Expr condition, Stmt body",
-                "Block      : List<Stmt> statements"
+                "Block      : List<Stmt> statements",
+                "Class      : Token name, List<Stmt.Function> methods",
+               "Expression : Expr expression"
         ));
     }
 
