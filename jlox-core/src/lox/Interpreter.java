50407414894a17412e2d47823af2e2b51b8f4775
diegolikescode <diegoprestes.git@hotmail.com>
2024-03-26 09:25:31 -0300

    getter and setter for classes

--- jlox-core/src/lox/Interpreter.java
+++ jlox-core/src/lox/Interpreter.java
@@ -194,6 +194,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name, "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
@@ -285,6 +298,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return function.call(this, arguments);
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, "Only instances have properties.");
     }
 
     private boolean isTruthy(Object object) {
--- jlox-core/src/lox/Resolver.java
+++ jlox-core/src/lox/Resolver.java
@@ -121,6 +121,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     }
 
     @Override
+    public Void visitGetExpr(Expr.Get expr) {
+        resolve(expr.object);
+        return null;
+    }
+
+    @Override
     public Void visitGroupingExpr (Expr.Grouping expr) {
         resolve(expr.expression);
         return null;
@@ -135,6 +141,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     public Void visitLogicalExpr (Expr.Logical expr) {
         resolve(expr.right);
         resolve(expr.left);
+        return null;
+    }
+
+    @Override
+    public Void visitSetExpr (Expr.Set expr) {
+        resolve(expr.value);
+        resolve(expr.object);
         return null;
     }
 
